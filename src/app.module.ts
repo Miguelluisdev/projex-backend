f13512849b41e98fbcef9089000270b70add4eed
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-03 19:56:23 -0300

    feat: add modules from modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphqlModule } from './graphql/graphql.module';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { NotificationModule } from './modules/notification/notification.module';
+import { PermissionModule } from './modules/permission/permission.module';
+import { ProjectModule } from './modules/project/project.module';
+import { TasksModule } from './modules/tasks/tasks.module';
 import { UserModule } from './modules/user/user.module';
+import { UserTypeModule } from './modules/user-type/user-type.module';
 
 @Module({
-  imports: [GraphqlModule, UserModule, PrismaModule],
+  imports: [
+    GraphqlModule,
+    UserModule,
+    PrismaModule,
+    ProjectModule,
+    TasksModule,
+    NotificationModule,
+    UserTypeModule,
+    PermissionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
