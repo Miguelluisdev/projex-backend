604c02dbb135bbfab26511c19fed905c8381ae8a
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-08 18:30:34 -0300

    feat: create a repository user

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -1,1 +1,46 @@
-//  aqui ficara o code first do graphql
+import { Injectable } from '@nestjs/common';
+import { UserMain } from '@prisma/client';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<UserMain[]> {
+    return await this.prismaService.userMain.findMany();
+  }
+
+  async findOneById(uuid: string): Promise<UserMain | null> {
+    return await this.prismaService.userMain.findUnique({
+      where: { uuid, deleted_at: null },
+    });
+  }
+
+  async findById(uuid: string): Promise<UserMain | null> {
+    return await this.prismaService.userMain.findFirst({
+      where: { uuid, deleted_at: null },
+    });
+  }
+
+  async findByEmail(email: string): Promise<UserMain | null> {
+    return await this.prismaService.userMain.findFirst({
+      where: { email, deleted_at: null },
+    });
+  }
+
+  async create(data: UserMain): Promise<UserMain> {
+    return await this.prismaService.userMain.create({ data });
+  }
+
+  async update(uuid: string, data: UserMain): Promise<UserMain> {
+    return await this.prismaService.userMain.update({
+      where: { uuid },
+      data,
+    });
+  }
+  async delete(uuid: string): Promise<UserMain> {
+    return await this.prismaService.userMain.delete({
+      where: { uuid, deleted_at: new Date() },
+    });
+  }
+}
