c08be8c3bfe16a7dd588c2d68769db390648397c
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-10 21:00:24 -0300

    feat: add auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphqlModule } from './graphql/graphql.module';
 import { PrismaModule } from './infra/database/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { NotificationModule } from './modules/notification/notification.module';
 import { PermissionModule } from './modules/permission/permission.module';
 import { ProjectModule } from './modules/project/project.module';
 import { TasksModule } from './modules/tasks/tasks.module';
-import { UserModule } from './modules/user/user.module';
 import { UserTypeModule } from './modules/user-type/user-type.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { UserTypeModule } from './modules/user-type/user-type.module';
     NotificationModule,
     UserTypeModule,
     PermissionModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
