91ad534ad32310ee98579526e84eee4527937170
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-14 15:57:02 -0300

    refactor: findAll

--- src/aplication/bases/services/service.base.ts
+++ src/aplication/bases/services/service.base.ts
@@ -1,9 +1,9 @@
-import { Prisma } from '@prisma/client';
+import { QueryParamsInput } from 'src/domain/dtos/shared/query-params.input';
 
 export abstract class ServiceBase<Entity, CreateDto = void, UpdateDto = void> {
   abstract create?(dto: CreateDto): Promise<Entity>;
   abstract findById(uuid: string): Promise<Entity>;
-  abstract findAll(): Promise<Entity>;
+  abstract findAll(queryParams: QueryParamsInput): Promise<Entity[]>;
   abstract update?(dto: UpdateDto): Promise<Entity>;
-  abstract remove?(id: string): Promise<boolean>;
+  abstract remove?(uuid: string): Promise<boolean>;
 }
