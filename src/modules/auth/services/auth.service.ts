2e6ae1cf9612ff363c6ee9deb708cb584cb482e5
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-16 03:36:20 -0300

    feat: create servive auth logic

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,27 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  forwardRef,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { addDays } from 'date-fns';
+import { ServiceBase } from 'src/aplication/bases/services/service.base';
 import { AuthInput } from 'src/domain/dtos/auth/auth.dto';
-import { CreateAuthInput } from 'src/domain/dtos/auth/create-auth.input';
 import { UpdateAuthInput } from 'src/domain/dtos/auth/update-auth.input';
+import { AuthEntity } from 'src/domain/entities/auth/auth.entity';
+import { UserService } from 'src/modules/user/services/user.service';
+import { AuthRepository } from '../repository/auth.repository';
 
 @Injectable()
-export class AuthService {
-  create(authInput: AuthInput) {
-    return 'This action adds a new auth';
+export class AuthService
+  implements ServiceBase<AuthEntity, AuthInput, UpdateAuthInput>
+{
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject(forwardRef(() => UserService))
+    private readonly userService: UserService,
+    private readonly authRepository: AuthRepository,
+  ) {}
+
+  async create(authInput: AuthInput): Promise<AuthEntity> {
+    const user = await this.userService.findByEmail(authInput.email);
+
+    const passwordIsEqual = await compare(authInput.password, user?.password);
+
+    if (!passwordIsEqual)
+      throw new HttpException('nao autorizado', HttpStatus.UNAUTHORIZED);
+
+    const auth = await this.justCreated(user.uuid);
+
+    return auth;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async justCreated(user_id: string): Promise<AuthEntity> {
+    const auth = await this.authRepository.create({
+      expires_at: addDays(new Date(), 15),
+      user_id,
+    });
+
+    const payload = { sub: auth.uuid };
+
+    const token = this.jwtService.sign(payload);
+
+    return {
+      ...auth,
+      token,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async findById(uuid: string): Promise<AuthEntity> {
+    const auth = await this.authRepository.findByid(uuid);
+
+    if (!auth) {
+      throw new HttpException(
+        'Atenticação não encontrada',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return auth;
   }
 
-  update(id: number, updateAuthInput: UpdateAuthInput) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  disconnect(uuid: string): Promise<AuthEntity> {
+    return this.authRepository.update({ uuid, expires_at: new Date() });
   }
 }
