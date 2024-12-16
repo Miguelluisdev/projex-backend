19e479cb16468b8dd524ccce8dbcd5242c9c814d
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-16 03:35:30 -0300

    refactor: repository

--- src/modules/auth/repository/auth.repository.ts
+++ src/modules/auth/repository/auth.repository.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { RepositoryFactory } from 'src/aplication/repositories/factories/repository.factory';
+import { CreateAuthInput } from 'src/domain/dtos/auth/create-auth.input';
+import { UpdateAuthInput } from 'src/domain/dtos/auth/update-auth.input';
 import { AuthEntity } from 'src/domain/entities/auth/auth.entity';
 import { QueryBuilderEntity } from 'src/domain/entities/builder/query-builder.entity';
-import { PrismaService } from 'src/infra/database/prisma/prisma.service';
 
 @Injectable()
-export class AuthRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+export class AuthRepository extends RepositoryFactory<
+  AuthEntity,
+  CreateAuthInput,
+  UpdateAuthInput
+> {
+  constructor() {
+    super('Auth');
+  }
 
   findAll(query: QueryBuilderEntity): Promise<AuthEntity[]> {
     return this.prismaService.auth.findMany(query);
   }
 
-  findById(id: string): Promise<AuthEntity | null> {
+  findByid(uuid: string): Promise<AuthEntity | null> {
     return this.prismaService.auth.findFirst({
-      where: { id, deleted_at: null },
+      where: {
+        uuid,
+        deleted_at: null,
+      },
     });
   }
 }
