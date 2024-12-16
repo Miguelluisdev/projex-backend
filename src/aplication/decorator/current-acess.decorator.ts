feed7bdf666fc1b0d30fb220a037be73178be878
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-16 03:33:49 -0300

    refactor: current acess for auth

--- src/aplication/decorator/current-acess.decorator.ts
+++ src/aplication/decorator/current-acess.decorator.ts
@@ -1,9 +1,18 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const CurrentAccess = createParamDecorator(
   (_data: unknown, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-
-    return request.auth;
+    const gqlContext = GqlExecutionContext.create(context);
+    const request = gqlContext.getContext();
+    if (!request.access) {
+      throw new HttpException('Access not found', HttpStatus.UNAUTHORIZED);
+    }
+    return request.access;
   },
 );
