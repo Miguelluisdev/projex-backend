5442694548a9f95fec789e5e923d046c06957640
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-16 03:32:54 -0300

    refactor: add array in findAll

--- src/aplication/bases/services/service.base.ts
+++ src/aplication/bases/services/service.base.ts
@@ -3,7 +3,7 @@ import { QueryParamsInput } from 'src/domain/dtos/shared/query-params.input';
 export abstract class ServiceBase<Entity, CreateDto = void, UpdateDto = void> {
   abstract create?(dto: CreateDto): Promise<Entity>;
   abstract findById(uuid: string): Promise<Entity>;
-  abstract findAll(queryParams: QueryParamsInput): Promise<Entity[]>;
+  abstract findAll?(queryParams: QueryParamsInput): Promise<Entity[]>;
   abstract update?(dto: UpdateDto): Promise<Entity>;
   abstract remove?(uuid: string): Promise<boolean>;
 }
