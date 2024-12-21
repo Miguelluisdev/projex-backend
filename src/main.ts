01e6cdc3288dd71698af7dcf9318bfe1e48a9965
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-21 20:17:31 -0300

    remove: remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,23 +12,7 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Projex-backend docs')
-    .setVersion('1.0')
-    .addTag('API-GRAPHQL')
-    .addBearerAuth(
-      {
-        description: 'Default JWT Authorization',
-        type: 'http',
-        in: 'header',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'DefaultBearerAuth',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+
 
   app.enableCors({
     origin: true,
