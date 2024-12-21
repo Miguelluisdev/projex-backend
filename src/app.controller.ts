454b030c7f0870f0aa96763d0d404a8e546eaeaf
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-21 18:29:06 -0300

    feat: deixando a rota publica

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { IsPublic } from './aplication/decorator/is-public.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
