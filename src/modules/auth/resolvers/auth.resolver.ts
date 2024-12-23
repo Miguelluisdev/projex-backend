621acf18c412a8f0b20b23263a57065849162b2f
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-12-23 00:18:46 -0300

    feat: add reset/password in resolver

--- src/modules/auth/resolvers/auth.resolver.ts
+++ src/modules/auth/resolvers/auth.resolver.ts
@@ -1,3 +1,4 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import {
   Args,
   Mutation,
@@ -9,11 +10,12 @@ import {
 import { CurrentAccess } from 'src/aplication/decorator/current-acess.decorator';
 import { IsPublic } from 'src/aplication/decorator/is-public.decorator';
 import { AuthInput } from 'src/domain/dtos/auth/auth.dto';
+import { ForgotPasswordInput } from 'src/domain/dtos/auth/forgot-password.input';
+import { ResetPasswordInput } from 'src/domain/dtos/auth/reset-password.input';
 import { AuthEntity } from 'src/domain/entities/auth/auth.entity';
 import { UserEntity } from 'src/domain/entities/user/user.entity';
 import { UserService } from 'src/modules/user/services/user.service';
 import { AuthService } from '../services/auth.service';
-import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Resolver(() => AuthEntity)
 export class AuthResolver {
@@ -28,14 +30,27 @@ export class AuthResolver {
     return this.authService.create(AuthInput);
   }
 
+  @Mutation(() => Boolean)
+  async forgotPasswordd(@Args('input') input: ForgotPasswordInput) {
+    return this.authService.forgotPassword(input);
+  }
+
+  @Mutation(() => String)
+  async resetPassword(@Args('input') input: ResetPasswordInput) {
+    return this.authService.resetPassword(input);
+  }
+
   @Query(() => AuthEntity)
   whoAmI(@CurrentAccess() auth: AuthEntity) {
     if (!auth) {
-      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
+      throw new HttpException(
+        'User not authenticated',
+        HttpStatus.UNAUTHORIZED,
+      );
     }
     return auth;
   }
-  
+
   @ResolveField(() => UserEntity, { nullable: true })
   @IsPublic()
   async user(@Parent() auth: AuthEntity) {
