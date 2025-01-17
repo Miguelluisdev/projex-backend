3fbda089e9a8ba95d6732ed45277444235f54bd0
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-16 22:13:27 -0300

    feat: create a dtos for project

--- src/domain/dtos/project/create-project.input.ts
+++ src/domain/dtos/project/create-project.input.ts
@@ -1,7 +1,42 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsDate, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { StatusProject } from 'src/domain/enums/status.project.enum';
 
 @InputType()
 export class CreateProjectInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  type_project: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  description: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  @Field(() => String)
+  start_date: Date;
+
+  @IsNotEmpty()
+  @IsDate()
+  @Field(() => String)
+  end_date: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  category: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Field(() => String)
+  goal: string;
+
 }
--- src/domain/dtos/project/update-project.input.ts
+++ src/domain/dtos/project/update-project.input.ts
@@ -1,8 +1,50 @@
-import { Field, InputType, Int, PartialType } from '@nestjs/graphql';
-import { CreateProjectInput } from 'src/domain/dtos/project/create-project.input';
+import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { IsDate, IsEnum, IsOptional, IsString, IsUUID } from 'class-validator';
+import { StatusProject } from 'src/domain/enums/status.project.enum';
+import { CreateProjectInput } from './create-project.input';
 
 @InputType()
 export class UpdateProjectInput extends PartialType(CreateProjectInput) {
-  @Field(() => Int)
-  id: number;
+  @IsUUID()
+  @IsString()
+  @Field(() => String, { description: 'ID único do projeto' })
+  uuid: string;
+
+  @IsOptional()
+  @IsString()
+  @Field(() => String, { nullable: true, description: 'Título do projeto' })
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  @Field(() => String, { nullable: true, description: 'Tipo do projeto' })
+  type_project?: string;
+
+  @IsOptional()
+  @IsString()
+  @Field(() => String, { nullable: true, description: 'Descrição do projeto' })
+  description?: string;
+
+  @IsOptional()
+  @IsDate()
+  @Field(() => String, { nullable: true, description: 'Data de término' })
+  end_date?: Date;
+
+  @IsOptional()
+  @IsString()
+  @Field(() => String, { nullable: true, description: 'Categoria do projeto' })
+  category?: string;
+
+  @IsOptional()
+  @IsString()
+  @Field(() => String, { nullable: true, description: 'Meta do projeto' })
+  goal?: string;
+
+  @IsOptional()
+  @IsEnum(StatusProject)
+  @Field(() => StatusProject, {
+    nullable: true,
+    description: 'Status do projeto',
+  })
+  status_project?: StatusProject;
 }
