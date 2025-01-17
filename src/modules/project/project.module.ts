d52e0062cfe9d2870d25843bc057d288c794d575
Miguel Luis <miguelluisataideferreira68@gmail.com>
2025-01-16 22:16:44 -0300

    feat: add project-repository in module

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProjectService } from '../project/services/project.service';
 import { ProjectResolver } from './resolvers/project.resolver';
+import { ProjectRepository } from './repositories/project.repository';
 
 @Module({
-  providers: [ProjectResolver, ProjectService],
+  providers: [ProjectResolver, ProjectService , ProjectRepository],
 })
 export class ProjectModule {}
